8bebd7ab8cfb7d8ade16ed80e4a261d2f9915744
Nanu Jogi <nanu.jogi@gmail.com>
2019-07-19 18:58:25 +0530

    updated CombinePattern

--- LearningCombineTests/CombinePattern.swift
+++ LearningCombineTests/CombinePattern.swift
@@ -359,7 +359,7 @@ class CombinePattern: XCTestCase {
         }
         print(type(of: publisher))  // CombineLatest<PassthroughSubject<String, Never>, PassthroughSubject<String, Never>>
     }
-
+    
     // Scan
     func testArlindCode7() {
         _ = Publishers.Sequence<[Int], Never>(sequence: [1,2,3,4,5])
@@ -390,4 +390,70 @@ class CombinePattern: XCTestCase {
         
         subject.send(99)
     }
+
+    // How to read detailDetail?
+    // ❌ test will be successful but not able to read detailDetail its an failure
+    func testJsonData() {
+        
+        struct Hub: Codable {
+            let id: Int
+            let title: String
+            let offPercent, store: String
+            let currentPrice: Int
+            let dealDetail: String
+            
+            enum CodingKeys: String, CodingKey {
+                case id, title
+                case offPercent = "off_percent"
+                case store
+                case currentPrice = "current_price"
+                case dealDetail = "deal_detail"
+            }
+        }
+        // setup Publisher is of type Data, Error
+        let simpleControlledPublisher = PassthroughSubject<Data, Error>()
+       
+        let _ = simpleControlledPublisher
+            .flatMap { value in // takes a String in and returns a Publisher
+                return Just(value)
+                    .decode(type: Hub.self, decoder: JSONDecoder())
+        }
+        .sink(receiveCompletion: { fini in
+            print(".sink() received the completion:", String(describing: fini))
+        }, receiveValue: { stringValue in
+            print(".sink() received \(stringValue)")
+        })
+        
+        let jsondata = """
+    {
+        "id": 948783,
+        "title": "Pay online using your Visa Signature or Visa Infinite Card and get 10% cashback up to ₹100 on one transaction during offer period.",
+        "off_percent": "",
+        "store": "Amazon",
+        "current_price": 0,
+        "deal_detail": "<p><a href=\"https://links.desidime.com?ref=forums&url=https://www.amazon.in/b/%3Fnode=15569417031\" target=\"_blank\" rel=\"nofollow\">https://www.amazon.in/b/?node=15569...31</a><br>\nFrequently Asked Question:<br>\n1. What is the offer?<br>\nPay online using your Visa Signature or Visa Infinite Card and get 10% cashback up to ₹100 on one transaction during offer period.</p>\n<p>2. What is the offer period?<br>\nDecember 20, 2018 to <del>December 31, 2018</del> September 30th 2019 (both days inclusive).</p>\n<p>3. Is there a minimum order value?<br>\nNo, there is no minimum order value.</p>\n<p>4. Is the offer available on all products?<br>\nThis offer is valid on mobile recharges, postpaid bill payments, utility payments (such as electricity, landline, broadband, gas, <span class=\"caps\">DTH</span> payments) as well as all purchases on Amazon.in except Amazon Pay Gift Cards, Amazon Pay balance, gold coins, <span class=\"caps\">RBL</span> bank cards, bank cards, Prime membership, precious jewelry, Kindle Unlimited Subscription, Kindle e-books, select Samsung mobile and tablet devices, tickets (travel, movie and event) and hotel bookings.</p>\n<p>5. When and where will the cashback be credited?<br>\nCashback will be credited as Amazon Pay balance within 3 business days from the date of shipping of the order placed on which the cashback offer is applied. However, for orders involving a product eligible for exchange and/or no-cost <span class=\"caps\">EMI</span>, the cashback will be provided within 20 days from the date of shipping (provided the order is not canceled, rejected or returned.)</p>\n<p>6.Is this offer available for Pay on Delivery?<br>\nNo, this offer cannot be used for Pay on Delivery, card on delivery or pay link on delivery.</p>\n<p>7.Can I place an order with multiple modes of payment?<br>\nNo, the complete payment for the Order(s) must be made using Visa Signature or Visa Infinite Card.</p>"
+    }
+""".data(using: .utf8)
+        simpleControlledPublisher.send(jsondata!)
+        simpleControlledPublisher.send(completion: .finished)
+    }
+    
+    func testSimpleMutating() {
+        struct Town {
+            var population = 5422
+            var numberOfStopLights = 4
+            
+            func printDescription() {
+                print("💥 Population : \(population) number of stoplights: \(numberOfStopLights)")
+            }
+            
+            mutating func changePopulation(by amount: Int) {
+                population += amount
+            }
+        }
+        var myTown = Town()
+        myTown.changePopulation(by: 500)
+        myTown.printDescription()
+    }
 }
+
